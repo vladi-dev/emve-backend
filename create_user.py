dbce11e602783cc0fd02b941baf9ef8abe432976
Vlad <email@email.com>
2015-01-10 14:44:25 -0800

    Update create_user.py

--- create_user.py
+++ create_user.py
@@ -1,7 +1,11 @@
-from app import db, user_datastore
+from app import app, db, user_datastore
+from flask_security.utils import encrypt_password
 
-db.drop_all()
-db.create_all()
-user_datastore.create_user(email='admin@emve.la', password='123123')
-db.session.commit()
+with app.app_context():
+    email = 'admin@emve.la'
+    password = encrypt_password('123123')
+    db.drop_all()
+    db.create_all()
+    user_datastore.create_user(email=email, password=password)
+    db.session.commit()
 
