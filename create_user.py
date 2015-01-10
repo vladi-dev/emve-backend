8f5ef08861b75b1ea06196a0d70714f8eaf83ab2
Vlad <email@email.com>
2015-01-10 14:29:48 -0800

    Update create_user.py

--- create_user.py
+++ create_user.py
@@ -2,6 +2,6 @@ from emwe import db, user_datastore
 
 db.drop_all()
 db.create_all()
-user_datastore.create_user(email='admin@emwe.com', password='123123')
+user_datastore.create_user(email='admin@emve.la', password='123123')
 db.session.commit()
 
