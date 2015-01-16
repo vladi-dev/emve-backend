14fb1eea3321588f8d2edfa9c8a0568ba985bfee
Vlad <email@email.com>
2015-01-15 17:25:20 -0800

    Add order to delivery

--- app/api/delivery.py
+++ app/api/delivery.py
@@ -13,19 +13,20 @@ class DeliveryAPI(MethodView):
     @jwt_required()
     def post(self):
         data = request.get_json(force=True)
+        order = data.get('order', None)
         address = data.get('address', None)
         contacts = data.get('contacts', None)
         establishment_id = data.get('establishment_id', None)
 
-        if not establishment_id:
-            return jsonify({'errors': {'_': 'Provide establishment'}}), 400
+        if not all(order, address, contacts, establishment_id):
+            return jsonify({'errors': {'_': 'Fill in all fields'}}), 400
 
         try:
             establishment = Establishment.query.filter_by(id=establishment_id).one()
         except Exception:
             return jsonify({'errors': {'_': 'Unknown establishment'}}), 400
 
-        d = Delivery(address=address, contacts=contacts, establishment_id=establishment.id, user_id=current_user.id)
+        d = Delivery(order=order, address=address, contacts=contacts, establishment_id=establishment.id, user_id=current_user.id)
         db.session.add(d)
         db.session.commit()
 
--- app/models/delivery.py
+++ app/models/delivery.py
@@ -4,5 +4,6 @@ class Delivery(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     establishment_id = db.Column(db.Integer, db.ForeignKey('establishment.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    order = db.Column(db.Text())
     address = db.Column(db.Text())
     contacts = db.Column(db.Text())
