717242b6f130da6b85ce997fe44a300238d2260a
Vlad <emve.dev@yandex.ru>
2015-06-02 15:40:51 -0800

    Extract websocket event service

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
-import json
 import redis
 import gevent
+import json
 
 from flask import Flask, render_template, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
@@ -8,7 +8,7 @@ from flask_security import Security, SQLAlchemyUserDatastore
 from flask_security.utils import verify_password
 from flask_admin import Admin
 from flask_cors import CORS
-from flask_jwt import JWT, current_user, jwt_required, verify_jwt
+from flask_jwt import JWT, current_user, verify_jwt
 from flask_uwsgi_websocket import GeventWebSocket
 
 
@@ -22,90 +22,6 @@ REDIS_CHAN = 'track'
 
 red = redis.StrictRedis
 redis = red.from_url(REDIS_URL)
-
-class TrackBackend(object):
-    """Interface for registering and updating WebSocket clients."""
-
-    def __iter_data(self):
-        for message in self.pubsub.listen():
-            data = message.get('data')
-            if message['type'] == 'message':
-                # app.logger.info(u'Sending message: {}'.format(data))
-                yield data
-
-    def register(self, client):
-        # app.logger.info(u'Registered...')
-        """Register a WebSocket connection for Redis updates."""
-        self.clients.append(client)
-
-    def send(self, client, data):
-        """Send given data to the registered client.
-        Automatically discards invalid connections."""
-        try:
-            # app.logger.info(u'Sending: ' + data)
-            client.send(data)
-        except Exception as e:
-            app.logger.info(u'Client removed')
-            self.clients.remove(client)
-
-    def run(self):
-        """Listens for new messages in Redis, and sends them to clients."""
-        self.clients = list()
-        self.pubsub = redis.pubsub()
-        self.pubsub.subscribe(REDIS_CHAN)
-        for data in self.__iter_data():
-            for client, id in self.clients:
-                if data:
-                    msg = json.loads(data)
-                    if not 'user_id' in msg or id == msg['user_id']:
-                        gevent.spawn(self.send, client, json.dumps(msg))
-
-    def start(self):
-        """Maintains Redis subscription in the background."""
-        gevent.spawn(self.run)
-
-track = TrackBackend()
-track.start()
-
-@ws.route('/websocket')
-def websocket(ws):
-    """Receives incoming messages, inserts them into Redis."""
-    with app.request_context(ws.environ):
-        try:
-            token = 'JWT ' + request.args.get('token')
-            verify_jwt(None, token)
-            track.register((ws, current_user.id))
-            while True:
-                # Sleep to prevent *contstant* context-switches.
-                gevent.sleep(0.1)
-                message = ws.receive()
-
-                if message is not None:
-                    if message == '':
-                        continue
-                    message = json.loads(message)
-
-                    if message['event'] == 'maven:coords_sent':
-                        status =  OrderStatus.getAccepted()
-
-                        try:
-                            order = Order.query.filter_by(maven_id=current_user.id, status_id=status.id).one()
-                        except Exception as e:
-                            continue
-
-                        message['order_id'] = order.id
-                        message['user_id'] = order.user_id
-                        message['event'] = 'client:track_order_{}'.format(order.id)
-                        redis.publish(REDIS_CHAN, json.dumps(message))
-                        app.logger.info(u'{}: {}'.format(message['event'], json.dumps(message)))
-                else:
-                    print 'break'
-                    break
-
-        except Exception as e:
-            print 'exception' + e.__unicode__()
-            return jsonify({'errors': e.__unicode__()}), 400
-
 
 # JWT Token Auth
 jwt = JWT(app)
@@ -153,7 +69,59 @@ adm.add_view(OrderModelView(db.session))
 from app.api.views import mod as api
 app.register_blueprint(api)
 
+# Websocket
+from app.websocket_service import WebsocketEventService
+
+ws_event_service = WebsocketEventService()
+ws_event_service.start()
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+# Websocket url for event service
+@ws.route('/websocket')
+def websocket(ws):
+    """Receives incoming messages, inserts them into Redis pubsub."""
+    with app.request_context(ws.environ):
+        try:
+            # Authenticate with JWT
+            token = 'JWT ' + request.args.get('token')
+            verify_jwt(None, token)
+            ws_event_service.register((ws, current_user.id))
+            while True:
+                # Sleep to prevent *contstant* context-switches.
+                gevent.sleep(0.1)
+                message = ws.receive()
+
+                if message is not None:
+                    # Ignore keep-alive messages
+                    if message == '':
+                        continue
+                    message = json.loads(message)
+
+                    # Process maven coordinates
+                    if message['event'] == 'maven:coords_sent':
+                        status = OrderStatus.getAccepted()
+
+                        # Find maven's current order
+                        try:
+                            order = Order.query.filter_by(maven_id=current_user.id, status_id=status.id).one()
+                        except Exception as e:
+                            continue
+
+                        # Compile message to client
+                        message['order_id'] = order.id
+                        message['user_id'] = order.user_id
+                        message['event'] = 'client:track_order_{}'.format(order.id)
+                        redis.publish(REDIS_CHAN, json.dumps(message))
+                        app.logger.info(u'{}: {}'.format(message['event'], json.dumps(message)))
+                else:
+                    print 'break'
+                    break
+
+        except Exception as e:
+            print 'exception' + e.__unicode__()
+            return jsonify({'errors': e.__unicode__()}), 400
---
+++ app/websocket_service.py
@@ -0,0 +1,46 @@
+import json
+import gevent
+
+from app import app, redis, REDIS_CHAN
+
+
+class WebsocketEventService(object):
+    """Interface for registering and updating WebSocket clients."""
+    def __iter_data(self):
+        for message in self.pubsub.listen():
+            data = message.get('data')
+            if message['type'] == 'message':
+                # app.logger.info(u'Sending message: {}'.format(data))
+                yield data
+
+    def register(self, client):
+        """Register a WebSocket connection for Redis updates."""
+        # app.logger.info(u'Registered...')
+        self.clients.append(client)
+
+    def send(self, client, data):
+        """Send given data to the registered client.
+        Automatically discards invalid connections."""
+        try:
+            # app.logger.info(u'Sending: ' + data)
+            client.send(data)
+        except Exception as e:
+            app.logger.info(u'Client removed')
+            self.clients.remove(client)
+
+    def run(self):
+        """Listens for new messages in Redis, and sends them to clients."""
+        self.clients = list()
+        self.pubsub = redis.pubsub()
+        self.pubsub.subscribe(REDIS_CHAN)
+        for data in self.__iter_data():
+            for client, id in self.clients:
+                if data:
+                    msg = json.loads(data)
+                    if not 'user_id' in msg or id == msg['user_id']:
+                        gevent.spawn(self.send, client, json.dumps(msg))
+
+    def start(self):
+        """Maintains Redis subscription in the background."""
+        gevent.spawn(self.run)
+
