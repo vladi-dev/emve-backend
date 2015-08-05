1eb8d8afeddfb124b04818d0ea71efd4746c2653
Emve Dev <emve.dev@yandex.ru>
2015-08-05 16:08:05 -0700

    Maven Signup

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from flask import redirect, url_for, request, flash
 from flask_admin import expose
 from flask_admin.babel import ngettext
@@ -9,27 +11,31 @@ from flask_security import current_user
 from app.models.user import User
 from app.models.user_address import UserAddress
 from app.models.order import Order
-from app.models.maven_account import MavenAccount
-
+from app.models.maven_account import MavenAccount, sexes
 
 
 class SecureModelViewMixin(object):
     def is_accessible(self):
         return current_user.is_authenticated()
 
+
 class SecureModelView(SecureModelViewMixin, ModelView):
     pass
 
+
 class SecureGeoModelView(SecureModelViewMixin, GeoModelView):
     pass
+
 
 class UserModelView(SecureModelView):
     def __init__(self, session, **kwargs):
         super(UserModelView, self).__init__(User, session, url='user', **kwargs)
 
+
 class UserAddressModelView(SecureGeoModelView):
     def __init__(self, session, **kwargs):
         super(UserAddressModelView, self).__init__(UserAddress, session, url='user_address', **kwargs)
+
 
 class OrderModelView(SecureModelView):
     can_create = False
@@ -43,6 +49,13 @@ class MavenAccountModelView(SecureModelView):
     can_edit = False
     can_delete = True
     can_view_details = True
+    column_labels = dict(ssn="Social Security Number", dob="Date of Birth", dl_state="Driver's License State",
+                         dl_number="Driver's License Number", account="Bank Account Number",
+                         routing="Bank Routing Number", bt_merch_acc_id="Braintree Merchant Account Id",
+                         bt_merch_acc_status="Braintree Merchant Account Status",
+                         bt_merch_acc_decline_reason="Braintree Merchant Account Decline Reason")
+    column_list = ['user', 'status', 'created_at']
+    column_formatters = dict(sex=lambda v, c, m, p: sexes[m.sex])
     details_template = 'admin/maven_signup_details.html'
 
     def __init__(self, session, **kwargs):
@@ -51,7 +64,7 @@ class MavenAccountModelView(SecureModelView):
     @expose('/approve/<int:id>', methods=('POST',))
     def approve(self, id):
         try:
-            maven_signup = MavenAccount.query.filter(MavenAccount.id==id).one()
+            maven_signup = MavenAccount.query.filter(MavenAccount.id == id).one()
             maven_signup.approve()
             flash('Maven account approved')
         except Exception as ex:
@@ -62,7 +75,7 @@ class MavenAccountModelView(SecureModelView):
     @expose('/decline/<int:id>', methods=('POST',))
     def decline(self, id):
         try:
-            maven_signup = MavenAccount.query.filter(MavenAccount.id==id).one()
+            maven_signup = MavenAccount.query.filter(MavenAccount.id == id).one()
             maven_signup.decline(request.form['decline_reason'])
             flash('Maven account declined')
         except Exception as ex:
