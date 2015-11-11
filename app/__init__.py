b91fc13240e5da36bc1e39d40802d543991df31d
Emve Dev <emve.dev@yandex.ru>
2015-11-12 00:11:21 +0200

    Disable redis console

--- app/__init__.py
+++ app/__init__.py
@@ -91,7 +91,7 @@ adm = Admin(app, name='Emve', template_mode='bootstrap2')
 
 from admin.views import UserModelView, UserAddressModelView, OrderModelView, MavenAccountModelView
 
-adm.add_view(rediscli.RedisCli(redis_client))
+# adm.add_view(rediscli.RedisCli(redis_client))
 adm.add_view(UserModelView(db.session))
 adm.add_view(UserAddressModelView(db.session))
 adm.add_view(OrderModelView(db.session))
